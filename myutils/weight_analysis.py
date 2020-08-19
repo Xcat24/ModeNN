234ebb50d856e4f098be71da4ea1f7a79ec23538
Xcat24 <1131551152@qq.com>
2020-08-19 19:31:44 +0800

    add weight analysis

--- myutils/weight_analysis.py
+++ myutils/weight_analysis.py
@@ -9,10 +9,27 @@ def find_term(idx, order=2, origin_dim=784):
     tmp = torch.combinations(torch.arange(origin_dim), order, with_replacement=True)
     return tmp[idx]
 
+def count_number(threshold1, threshold2, x):
+    x = torch.abs(x)
+    if threshold1 == None:
+        return torch.where(x<threshold2, torch.ones(x.shape, device=x.device, dtype=torch.int), torch.zeros(x.shape, device=x.device, dtype=torch.int)).sum().item()
+    elif threshold2 == None:
+        return torch.where(x>=threshold1, torch.ones(x.shape, device=x.device, dtype=torch.int), torch.zeros(x.shape, device=x.device, dtype=torch.int)).sum().item()
+    else:
+        return torch.where(torch.logical_and(x>=threshold1, x<threshold2), torch.ones(x.shape, device=x.device, dtype=torch.int), torch.zeros(x.shape, device=x.device, dtype=torch.int)).sum().item()
+
 if __name__ == '__main__':
     path = '/home/xucong/Log/MNIST/ModeNN/2order/best_98.35.ckpt'
     weight_name = 'fc.weight'
     w = load_model_weight(path, weight_name)
+    print(count_number(0, 0.01, w))
+    print(count_number(0.01, 0.1, w))
+    print(count_number(0.1, 0.15, w))
+    print(count_number(0.15, 0.2, w))
+    print(count_number(0.2, 0.25, w))
+    print(count_number(0.25, 0.3, w))
+    print(count_number(0.3, 0.35, w))
+    print(count_number(0.35, None, w))
     # data_statics('2order-weight', w, verbose=True)
     feature_dim_norm = torch.norm(w, dim=0)
     # data_statics('feature_dim_norm', feature_dim_norm, verbose=True)
